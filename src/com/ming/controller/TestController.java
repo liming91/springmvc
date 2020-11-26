ea1a4b3af15c19cb4e7c1efd2b8dd6b58c1af1ba
liming <522724933@qq.com>
2020-11-26 17:23:39 +0800

    #fist mvc

---
+++ src/com/ming/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class TestController {
+    @RequestMapping("/index")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        ModelAndView model=new ModelAndView("index");
+        model.addObject("message","这是第一个SpringMVC网页");
+        return model;
+    }
+}
